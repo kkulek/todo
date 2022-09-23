b99dff3f5f981dcc3dcc12e47532173172e85e8e
kkulek <krzysztof.kulka@hotmail.com>
2022-09-23 14:34:20 +0200

    updated Footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,20 +1,11 @@
+import Counter from "./Counter";
+import Filters from "./Filters";
+
 function Footer({tasks, filters, setFilters, handleDeleteCompleted}){
     return (
         <footer>
-            <div>
-                {tasks.filter((task) => !task.status).length} items left
-            </div>
-            <div>
-                <button className={filters === 'all' ? 'filter-active' : ''}
-                        onClick={() => setFilters('all')}>All
-                </button>
-                <button className={filters === false ? 'filter-active' : ''}
-                        onClick={() => setFilters(false)}>Active
-                </button>
-                <button className={filters === true ? 'filter-active' : ''}
-                        onClick={() => setFilters(true)}>Completed
-                </button>
-            </div>
+            <Counter tasks={tasks}/>
+            <Filters filters={filters} setFilters={setFilters}/>
 
             {tasks.some((task) => task.status) && (
                 <div>
